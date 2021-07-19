cbac2afeeb09cb2ec8b9520084b0fe1f126769cc
nkc20206036 <kt2053@mailg.denpa.ac.jp>
2021-07-19 12:44:04 +0900

    project+

---
+++ Coins/Assets/Pushuer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pushuer : MonoBehaviour
+{
+    Vector3 PushuerPosition;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PushuerPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float z = Mathf.Sin(Time.time * speed);
+        transform.position = PushuerPosition + new Vector3(0, 0, z);
+    }
+}
---
+++ Coins/Assets/Shoot.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+
+    public GameObject medalPrefab;
+    private float span = 1.0f;
+    private float lefttime, righttime = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lefttime += Time.deltaTime;
+        righttime += Time.deltaTime;
+    }
+
+    public void LeftClick()
+    {
+        if (span < lefttime)
+        {
+            GameObject medal = Instantiate(medalPrefab, new Vector3(2.9f, 9.9f, 2.1f), Quaternion.Euler(90f, 110f, 0f));
+            lefttime = 0;
+        }
+    }
+
+    public void RightClick()
+    {
+        if (span < righttime)
+        {
+            GameObject medal = Instantiate(medalPrefab, new Vector3(-2.9f, 9.9f, 2.1f), Quaternion.Euler(90f, -110f, 0f));
+            righttime = 0;
+        }
+    }
+
+}
